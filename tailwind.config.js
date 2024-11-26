bec8764f724d317d2d5676cc293fc016c9919d6d
rjaya <deveo1616@gmail.com>
2024-11-26 06:50:26 +0530

    working

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,22 @@ module.exports = {
       backgroundImage:{
          'try': "url('./assets/10 SCENE.png')",
                   'desert': "url('./assets/desert.png')",
-                  'statue':"url('./assets/landscape/DrawKit Vector Illustration Landscape & Scenery (3).png')"
+                  'statue':"url('./assets/landscape/DrawKit Vector Illustration Landscape & Scenery (3).png')",
+                  'landscape1':"url('./assets/landscape/DrawKit Vector Illustration Landscape & Scenery (3).png')",
+                  'landscape2':"url('./assets/landscape/DrawKit Vector Illustration Landscape & Scenery (2).png')",
+'landscape3': "linear-gradient( rgba(0,0,0,0.5) , rgba(0,0,0,0.5)), url('./assets/landscape/DrawKit Vector Illustration Landscape & Scenery (4).png')",
+                  'landscape4':"url('./assets/landscape/DrawKit Vector Illustration Landscape & Scenery (5).png')",
+                  'landscape5':"url('./assets/landscape/DrawKit Vector Illustration Landscape & Scenery (6).png')",
+                  'landscape6':"url('./assets/landscape/DrawKit Vector Illustration Landscape & Scenery (7).png')",
+                  'landscape7':"url('./assets/landscape/DrawKit Vector Illustration Landscape & Scenery (8).png')",
+                  'landscape8':"url('./assets/landscape/DrawKit Vector Illustration Landscape & Scenery (9).png')",
+                  'landscape9':"url('./assets/landscape/DrawKit Vector Illustration Landscape & Scenery (10).png')",
+                  'landscape10':"url('./assets/landscape/DrawKit Vector Illustration Landscape & Scenery (11).png')",
+                  'landscape11':"url('./assets/landscape/DrawKit Vector Illustration Landscape & Scenery 12).png')",
+                  'landscape12':"url('./assets/landscape/DrawKit Vector Illustration Landscape & Scenery (14).png')",
+                  'landscape13':"url('./assets/landscape/DrawKit Vector Illustration Landscape & Scenery (15).png')",
+                  'hero':"url('./assets/Desktop - 1.png')",
+                  
       },
       keyframes: {
         'border-spin': {
